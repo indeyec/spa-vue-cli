cb4216bd400201c4b5ec833045a8dd6ca036499e
Vladislav Shilov <vlad.shilov03@mail.ru>
2023-09-13 23:47:31 +0700

    product and nav create templates

---
+++ src/components/Product.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="cardProduct">
+      <h2>Каталог</h2>
+      <div class="cards">
+        <div v-for="card in allProducts.data">
+          <h3>{{ card.name }}</h3>
+          <p>{{ card.description }}</p>
+          <div>
+            <span><b>{{ card.price }} &#8381;</b></span>
+            <button v-if="this.$store.getters.isAuthenticated" @click="toCart(card)">In the cart...</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "CardProduct",
+    props: ['allProducts'],
+    methods: {
+      toCart(card) {
+        this.$store.dispatch('to_cart', card.id)
+        this.$store.dispatch('get_cart', card.id)
+      }
+    },
+  }
+  </script>
+  
+  <style scoped>
+  .cards {
+    display: flex;
+    flex-wrap: wrap;
+    width: 60%;
+    margin: 0 auto;
+  }
+  .cards > div {
+    width: 40%;
+    padding: 10px 20px 40px 20px;
+    margin: 30px auto;
+    border: 2px solid #2c3e50;
+    border-radius: 10px;
+    position: relative;
+  }
+  
+  .cards > div:hover {
+    border: 2px solid rgb(107, 36, 248);
+  }
+  
+  .cards > div > h3 {
+    text-align: left;
+    text-indent: 10px;
+  }
+  
+  .cards > div > span {
+    position: absolute;
+    left: 20px;
+    bottom: 15px;
+  }
+  
+  .cards > div > div {
+    width: 91%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: absolute;
+    bottom: 15px;
+  }
+  
+  .cards > div > p {
+    text-align: left;
+    text-indent: 10px;
+    margin-bottom: 20px;
+  }
+  
+  .cards > div > div > button {
+    color: #2c3e50;
+    width: 120px;
+    height: 32px;
+    font-size: 16px;
+    border: 2px solid #2c3e50;
+    border-radius: 5px;
+    background: rgba(116, 134, 227, 0.8);
+    cursor: pointer;
+  }
+  </style>
---
+++ src/components/navigation.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="header">
+      <h1>My practice shop!</h1>
+      <div class="nav">
+        <router-link to="catalog"><p>Catalog</p></router-link>
+        <router-link to="cart"><p>Cart ({{ this.$store.state.cartCount }})</p></router-link>
+        <router-link to="orders"><p>Orders</p></router-link>
+        <router-link v-if="!this.$store.getters.isAuthenticated" to="login"><p>Sign In</p></router-link>
+        <router-link v-if="!this.$store.getters.isAuthenticated" to="register"><p>Sign Up</p></router-link>
+        <router-link v-if="this.$store.getters.isAuthenticated" to="logout" @click="logout"><p>Logout...</p></router-link>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "header",
+    methods: {
+      logout() {
+        this.$store.dispatch('logout')
+      },
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .header {
+    display: inline-block;
+    margin-left: 20px;
+    justify-content: space-around;
+    align-items: center;
+    margin-top: 15px;
+    margin-bottom: 40px;
+  }
+  .header > h1 {
+   text-align: center;
+    animation: pulsate 0.11s ease-in-out infinite alternate;
+    text-shadow:
+        0 0 7px #fff,
+        0 0 10px #fff,
+        0 0 21px #fff,
+        0 0 42px #945fff,
+        0 0 82px #5e00ff,
+        0 0 92px #5e00ff,
+        0 0 102px #5e00ff,
+        0 0 151px #5e00ff;
+  }
+  
+  .nav {
+    margin-top: 90px;
+    font-size: 21px;
+    margin-bottom: 20px;
+    width: 500px;
+    display: flex;
+    justify-content: space-around;
+  }
+  
+  a {
+    font-weight: bold;
+    color: #213c4f;
+    text-decoration: none;
+  }
+  
+  a.router-link-exact-active {
+    color: rgb(57, 36, 248);
+  }
+  </style>
+  <style>
+  *{
+    background-color: #dec8ff;
+    margin: 0;
+    padding: 0;
+  }
+  </style>
