93dbf1b9b14bb03b4d07dc09789111dbb5b077e5
Vladislav Shilov <vlad.shilov03@mail.ru>
2023-09-13 23:37:26 +0700

    Create orders component

---
+++ src/views/Orders.vue
@@ -0,0 +1,52 @@
+<template>
+    <h1>Orders page!</h1>
+    <div v-if="this.$store.state.orders.length > 0">
+    <div class="card" v-for="card in $store.state.orders">
+      <br>
+      <div>
+        <p>Your oder #{{ card.id }}</p>
+        <div>
+          <p>{{ card.order_price }} &#8381;</p>
+        </div>
+      </div>
+      <hr>
+    </div>
+    </div>
+    <div v-else>
+      <p>You have 0 orders!</p>
+      <br>
+      <p>Order makes from products in the cart, so you should check it!</p>
+      <router-link to="cart"><p>Cart</p></router-link>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "OrderView",
+    mounted() {
+      this.$store.dispatch('get_order')
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .card {
+    width: 1000px;
+    margin: 0 auto;
+  }
+  
+  .card > div {
+    display: flex;
+    justify-content: space-between;
+    padding: 0 30px;
+    align-items: center;
+  }
+  
+  .card > div > div {
+    width: 80px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  
+  </style>
