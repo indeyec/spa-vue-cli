27136296d311dfba329446a69410200cedcfe3b7
Vladislav Shilov <vlad.shilov03@mail.ru>
2023-09-13 23:37:03 +0700

    Create Catalog component

---
+++ src/views/Catalog.vue
@@ -0,0 +1,29 @@
+<template>
+    <card-product :allProducts="allProducts"/>
+  </template>
+  
+  <script>
+  import CardProduct from "@/components/Product.vue";
+  import axios from "axios";
+  
+  export default {
+    name: "catalog",
+    components: {
+      CardProduct
+    },
+    data() {
+      return {
+        allProducts: []
+      }
+    },
+    mounted() {
+      axios.get(this.$store.state.API + 'products').then((response) => {
+        this.allProducts = response.data
+      })
+    }
+  }
+  </script>
+  
+  <style scoped>
+  
+  </style>
