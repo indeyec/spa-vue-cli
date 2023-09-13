731602cee181935c002b8f2c1a912e32c9c495fb
Vladislav Shilov <vlad.shilov03@mail.ru>
2023-09-13 23:43:19 +0700

    Routers create on component

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,71 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import CartView from '../views/Cart.vue'
+import OrdersView from '../views/Orders.vue'
+import RegisterView from '../views/SignUp.vue'
+import LoginView from '../views/Login.vue'
+import CatalogView from '../views/Catalog.vue'
+
+import store from '../store'
+
+const ifNotAuthenticated = (to, from, next) => {
+  if (!store.getters.isAuthenticated) {
+    next()
+    return
+  }
+  next('/')
+}
+
+const ifAuthenticated = (to, from, next) => {
+  if (store.getters.isAuthenticated) {
+    next()
+    return
+  }
+  next('/login')
+}
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '',
+    redirect: {name: 'catalog'}
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
-  }
+    path: '/cart',
+    name: 'cart',
+    component: CartView,
+    beforeEnter: ifAuthenticated
+  },
+  {
+    path: '/catalog',
+    name: 'catalog',
+    component: CatalogView,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView,
+    beforeEnter: ifNotAuthenticated
+  },
+  {
+    path: '/orders',
+    name: 'orders',
+    component: OrdersView,
+    beforeEnter: ifAuthenticated
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterView,
+    beforeEnter: ifNotAuthenticated,
+  },
+  {
+    path: '/logout',
+    redirect: {name: 'login'}
+  },
 ]
 
-const router = createRouter({
+const index = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
-export default router
+export default index
